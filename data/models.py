e877a968d82ae39496ba1aea03131b3e99856a19
birkata <birkata@yahoo.com>
2025-04-23 13:59:53 +0300

    Initial skeleton and DB connection

---
+++ data/models.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+class Messages(BaseModel):
+    pass
+
+class Users(BaseModel):
+    pass
+
+class Categories(BaseModel):
+    pass
+
+class Replies(BaseModel):
+    pass
+
+class Topics(BaseModel):
+    pass
+
