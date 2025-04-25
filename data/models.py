d8e086118c45cab084a35b7e17ce94ad0777ba9d
PetarKrasimirovPavlov <petar.pavlov.a69@learn.telerikacademy.com>
2025-04-25 13:41:53 +0300

    Messages and Users models added

--- data/models.py
+++ data/models.py
@@ -1,10 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, constr, 
+from datetime import datetime
+
 
 class Messages(BaseModel):
-    pass
+    id: int | None
+    text: str
+    sent_on: datetime | None = datetime.now.strftime('%Y-%m-%d %H:%M:%S')
 
 class Users(BaseModel):
-    pass
+    id: int | None
+    username: constr(min_length=6, max_length=30)
+    email: str
+    password: constr(min_length=6, max_length=30)
+    bio: str | None
+    is_admin: bool | None = False
+    is_active: bool | None = True
 
 class Categories(BaseModel):
     pass
