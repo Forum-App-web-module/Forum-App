c10ef4c359f2dac3006400a3e5263046d611a739
PetarKrasimirovPavlov <petar.pavlov.a69@learn.telerikacademy.com>
2025-04-26 18:50:31 +0300

    database back to Admin

--- data/database.py
+++ data/database.py
@@ -5,7 +5,7 @@ from mariadb.connections import Connection
 def _get_connection() -> Connection:
     return connect(
         user='root',
-        password='1997',
+        password='Admin',
         host='localhost',
         port=3306,
         database='forum_app'
