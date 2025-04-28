9c778f0332ba15342fd7fd45b10ba3067d7fc2df
birkata <birkata@yahoo.com>
2025-04-28 16:31:22 +0300

    upvote / downvote endpoint and service

--- routers/replies.py
+++ routers/replies.py
@@ -25,6 +25,26 @@ def create_reply(topic_id: int, reply: str = Body(..., min_length=1, max_length=
 
     return {"message": "Reply created successfully"}
 
+@replies_router.post("/{topic_id}/vote/{reply_id}", status_code=201)
+def vote_on_reply(topic_id: int, reply_id: int, vote: str = Body(regex='^(-1|1)$'), token: str = Header()):
+    # token verification
+    try:
+        payload = verify_access_token(token)
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    # DB creator_id extract from token
+    user_id = payload["key"]["id"]
+
+    try:
+        result = reply_service.vote_on_r(topic_id, reply_id, user_id, vote)
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+
+    if result:
+        return {"message": "Vote was updated. Long live democracy!"}
+
+
 
 
 
--- services/reply_service.py
+++ services/reply_service.py
@@ -1,4 +1,4 @@
-from data.database import insert_query
+from data.database import insert_query, query_count, update_query
 from data.models import Replies
 
 def create_reply(reply: str, topic_id: int, user_id: int):
@@ -18,3 +18,39 @@ def create_reply(reply: str, topic_id: int, user_id: int):
         raise ValueError("Reply text and topic id are required")
 
     return True
+
+def vote_on_r(topic_id: int, reply_id: int, user_id: int, vote: str):
+    # validate user exists
+    user_exists = query_count("SELECT COUNT(*) FROM users WHERE id = ?", (user_id,))
+    if user_exists == 0:
+        raise ValueError("User does not exist.")
+
+    # validate reply belongs to topic
+    query = """
+        select * from replies
+        where id = ?
+        and topic_id = ?
+        """
+
+    reply_exists = query_count(query, (reply_id, topic_id))
+
+    if reply_exists == 0:
+        raise ValueError("Wrong parameters. Reply does not exist or does not belong to the topic.")
+
+    # record the vote in the DB
+    vote_data = query_count(
+        "select * from votes where users_id = ? and replies_id = ?", (user_id, reply_id))
+
+    if vote_data > 0:
+        update_query("update votes set vote = ? where users_id = ? and replies_id = ?", (int(vote), user_id, reply_id))
+    else:
+        insert_query("insert into votes (users_id, replies_id, vote) values (?, ?, ?)", (user_id, reply_id, int(vote)))
+
+    return True
+
+
+
+
+
+
+
