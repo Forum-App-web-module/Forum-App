64bb1342bb44c6fa7b9fb4478c64aa2df794f47b
birkata <birkata@yahoo.com>
2025-04-28 16:50:46 +0300

    adding description to routers

--- routers/replies.py
+++ routers/replies.py
@@ -7,7 +7,20 @@ from security.jwt_auth import verify_access_token
 replies_router = APIRouter(prefix='/replies', tags=['Replies'])
 
 @replies_router.post("/{topic_id}", status_code=201)
-def create_reply(topic_id: int, reply: str = Body(..., min_length=1, max_length=400), token: str = Header()):
+def create_reply(
+        topic_id: int,
+        reply: str = Body(..., min_length=1, max_length=400),
+        token: str = Header()
+):
+    """
+      Create a reply under a specific topic.
+
+      - **topic_id**: ID of the topic where the reply is posted
+      - **reply**: Text content of the reply (1-400 characters)
+      - **token**: User's access token (JWT) in the header for authentication
+
+      Returns a success message upon creation.
+    """
     # token verification
     try:
         payload = verify_access_token(token)
@@ -26,7 +39,23 @@ def create_reply(topic_id: int, reply: str = Body(..., min_length=1, max_length=
     return {"message": "Reply created successfully"}
 
 @replies_router.post("/{topic_id}/vote/{reply_id}", status_code=201)
-def vote_on_reply(topic_id: int, reply_id: int, vote: str = Body(regex='^(-1|1)$'), token: str = Header()):
+def vote_on_reply(
+        topic_id: int,
+        reply_id: int,
+        vote: str = Body(regex='^(-1|1)$'),
+        token: str = Header()
+):
+    """
+       Vote on a specific reply.
+
+       - **topic_id**: ID of the topic containing the reply
+       - **reply_id**: ID of the reply to vote on
+       - **vote**: Must be `-1` (downvote) or `1` (upvote)
+       - **token**: User's access token (JWT) in the header for authentication
+
+       Returns a success message after voting.
+    """
+
     # token verification
     try:
         payload = verify_access_token(token)
