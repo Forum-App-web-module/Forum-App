367f55157aec8b40a8ec3a5dd6425ccf8004e7d4
dilyanab <bozhinova.dilyana@gmail.com>
2025-04-29 04:57:53 +0300

    adding post methods and auth for topics and categories

--- routers/categories.py
+++ routers/categories.py
@@ -3,18 +3,22 @@ from services import category_service
 
 category_router = APIRouter(prefix="/categories", tags=["Categories"])
 
+#view all categories
 @category_router.get('/')
 def view_categories():
-    return category_service.get_all
+    categories = category_service.get_all()
+    return categories
 
+#view category by ID
 @category_router.get('/{category_id}')
 def view_category(category_id: int):
     categroy = category_service.get_category_by_id(category_id)
     if not categroy:
-        return Response(content='{"message":"No tcategory found for this id"}', status_code=404)
+        return Response(content='{"message":"No category found for this id"}', status_code=404)
     
     return categroy
 
+#View topics for a category
 @category_router.get('/{category_id}/topics')
 def view_category_topics(category_id: int):
     topics = category_service.get_topics_by_category(category_id)
--- routers/topics.py
+++ routers/topics.py
@@ -1,8 +1,28 @@
-from fastapi import APIRouter, Response
-from services import topic_service
+from fastapi import APIRouter, Response, Header, Body
+from services import topic_service, reply_service
+from security.jwt_auth import verify_access_token
 
 topic_router = APIRouter(prefix='/topics', tags=["Topics"])
 
+# create a new topic
+@topic_router.post('/')
+def create_topic(
+    title: str = Body(..., min_length=1, max_length=200), 
+    category_id: int = Body(...), 
+    token: str = Header()
+    ):
+    payload = verify_access_token(token)
+    if not payload:
+        return Response(content='{"message":"Authentication failed."}', status_code=401)
+    
+    author_id = payload['id']
+    new_id = topic_service.create_topic(title, category_id, author_id)
+
+    if new_id:
+        return Response(content=f'{"message":"Topic {title} created"}', status_code=201)
+    return Response(content="Failed to create topic", status_code=500)  #fix status code
+
+#view all topics, no authentication needed
 @topic_router.get('/')
 def view_topics():
     topics = topic_service.get_all_topics()
@@ -11,10 +31,27 @@ def view_topics():
     
     return topics
 
+#vieww topic by id, show replies
 @topic_router.get('/{topic_id}')
 def view_topic_by_id(topic_id: int):
-    topic = topic_service.get_topic_by_id(topic_id)
-    if not topic: 
+    topic_replies = topic_service.get_topic_with_replies(topic_id)
+    if not topic_replies: 
         return Response(content='{"messgae":"No topic found for the given ID"}', status_code=404)
     
-    return topic
+    return topic_replies
+
+#create a reply for a topic, auth required
+@topic_router.post('/{topic_id}/replies', status_code=201)
+def create_reply(
+    topic_id: int,
+    text: str = Body(...,min_length=1, max_length=400),
+        token: str = Header()
+):
+    payload = verify_access_token(token)
+    if not payload:
+        return Response(content='{"messgae":"Authentication failed"}', status_code=401)
+    
+    user_id = payload['id']
+    reply_service.create_reply(text, topic_id, user_id)
+
+    return Response(content='{"messgae":"Reply created"}', status_code=201)
