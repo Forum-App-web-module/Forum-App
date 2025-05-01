5d3abf275970766695392481fc5a8d8168d7176e
birkata <birkata@yahoo.com>
2025-05-01 22:00:59 +0300

    basic checks from SHOULD

--- services/category_members_service.py
+++ services/category_members_service.py
@@ -1,14 +1,27 @@
+from data.database import query_count, insert_query
 
 
-# TODO: Boby
-def is_member(category_id, user_id):
-    pass
+def is_member(category_id: int, user_id: int) -> bool:
+    query = "SELECT * FROM category_members WHERE category_id = ? AND user_id = ?"
+
+    data = query_count(query, (category_id, user_id))
+
+    return True if data == 1 else False
 
 def can_write(category_id, user_id):
-    pass
+    query = "SELECT * FROM category_members WHERE category_id = ? AND user_id = ? AND write_access = 1"
+
+    data = query_count(query, (category_id, user_id))
+
+    return True if data == 1 else False
 
 def give_access(category_id, user_id):
-    pass
+    query = "INSERT INTO category_members (category_id, user_id, write_access) VALUES (?, ?, 1)"
+
+    data = insert_query(query, (category_id, user_id))
+
+    return True if data else False
+
 
 # TODO: Pesho
 def revoke_access(category_id, user_id): 
