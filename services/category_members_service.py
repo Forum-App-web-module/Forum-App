a0e28223b8d9a3a84b1f859277daa3c7fa574c36
PetarKrasimirovPavlov <petar.pavlov.a69@learn.telerikacademy.com>
2025-05-04 12:43:09 +0300

    category_members refactor

--- services/category_members_service.py
+++ services/category_members_service.py
@@ -1,4 +1,5 @@
 from data.database import query_count, insert_query
+from data import database
 
 
 def is_member(category_id: int, user_id: int) -> bool:
@@ -24,11 +25,41 @@ def give_access(category_id, user_id):
 
 
 # TODO: Pesho
-def revoke_access(category_id, user_id): 
-    pass
+def revoke_access(category_id, user_id, update_func = None):
 
-def update_write_access(category_id, user_id, access: bool):
-    pass
+    if update_func is None:
+        update_func = database.update_query
 
-def view_privileged_users(category_id):
-    pass
+    query = 'DELETE from category_members WHERE category_id = ? AND user_id=?'
+
+    data = update_func(query, (category_id, user_id))
+
+    return True if data else False
+    
+
+def update_write_access(category_id, user_id, access: bool, update_func = None):
+    
+    if update_func is None:
+            update_func = database.update_query
+    
+    if access == True: 
+        updated_access = 0
+    else: updated_access = 1
+
+    query = 'UPDATE category_members SET write_access = ? WHERE category_id = ? AND user_id=?'
+
+    data = update_func(query, (updated_access, category_id, user_id))
+
+    return True if data else False
+
+
+def view_privileged_users(category_id, get_data_func = None):
+     
+    if get_data_func is None:
+            get_data_func = database.read_query
+
+    query = 'SELECT users.username FROM users JOIN category_members as cat ON users.id = cat.user_id WHERE cat.category_id = ?'
+    
+    data = get_data_func(query, (category_id,))
+
+    return data
