811652a011da4402e9bf3e81248d815833945623
PetarKrasimirovPavlov <petar.pavlov.a69@learn.telerikacademy.com>
2025-05-12 15:14:45 +0300

    refactor

--- security/secrets.py
+++ security/secrets.py
@@ -2,7 +2,6 @@ from data.database import query_count
 from hashlib import sha256
 from dotenv import load_dotenv
 from os import getenv
-from services.user_service import validate_user
 
 load_dotenv(dotenv_path="key_example.env")
 
