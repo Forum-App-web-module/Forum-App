5e4942d31f443696bc798ecf907b5e7e9488f915
PetarKrasimirovPavlov <petar.pavlov.a69@learn.telerikacademy.com>
2025-05-16 15:25:54 +0300

    refactor macro

--- routers/web/categories.py
+++ routers/web/categories.py
@@ -6,9 +6,10 @@ from fastapi.templating import Jinja2Templates
 from common.auth import get_user_if_token
 from security.jwt_auth import verify_access_token
 from services.category_service import get_all_public, get_all, get_allowed
+from common.template_config import CustomJinja2Templatges
 
 category_router = APIRouter(prefix='')
-templates = Jinja2Templates(directory="templates")
+templates = CustomJinja2Templatges(directory="templates")
 
 
 @category_router.get('/categories')
@@ -18,13 +19,13 @@ def serve_categories(request: Request):
 
     if not payload:
         categories = get_all_public()
-        return templates.TemplateResponse(request=request, name="prefixed/categories.html",context={"categories": categories})
+        return templates.TemplateResponse(request=request, name="/prefixed/categories.html",context={"categories": categories})
 
     if payload["key"]["is_admin"]:
         categories = get_all()
-        return templates.TemplateResponse(request=request, name="prefixed/categories.html", context={"categories": categories})
+        return templates.TemplateResponse(request=request, name="/prefixed/categories.html", context={"categories": categories})
     else:
         categories = get_allowed(payload["key"]["id"])
-        return templates.TemplateResponse(request=request, name="prefixed/categories.html", context={"categories": categories})
+        return templates.TemplateResponse(request=request, name="/prefixed/categories.html", context={"categories": categories})
 
 
